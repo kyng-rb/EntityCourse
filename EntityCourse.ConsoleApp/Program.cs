fe484a07042507e72adee5a22cc05d5a86c88c07
Reynaldo Rodriguez <rodriguezreynaldo1698@outlook.com>
2022-05-20 16:35:05 -0600

    Added search methods

--- EntityCourse.ConsoleApp/Program.cs
+++ EntityCourse.ConsoleApp/Program.cs
@@ -1,15 +1,19 @@
 ﻿using EntityCourse.Data.DatabaseContext;
 using EntityCourse.Domain.Models;
+using Microsoft.EntityFrameworkCore;
 
-void Test()
+async Task Test()
 {
-    FootballLeagueDbContext dbContext = new FootballLeagueDbContext();
-    var record = new League("Geek League");
-    dbContext.Leagues.Add(record);
-    dbContext.SaveChanges();
+    //FootballLeagueDbContext dbContext = new FootballLeagueDbContext();
+    //var record = new League("Geek League");
+    //dbContext.Leagues.Add(record);
+    //dbContext.SaveChanges();
 
-    InsertManyTeams();
-    InsertRelated();
+    //InsertManyTeams();
+    //InsertRelated();
+
+    await Retrieve();
+    await RetrieveWithFilter();
     Console.ReadKey();
 }
 
@@ -63,4 +67,54 @@ void InsertRelated()
     dbContext.SaveChanges();
 }
 
-Test();
+async Task Retrieve()
+{
+    FootballLeagueDbContext dbContext = new FootballLeagueDbContext();
+
+    var leagues = await dbContext.Leagues.ToListAsync();
+    foreach (var league in leagues)
+    {
+        Console.WriteLine($"{league.Id} - {league.Name}");
+    }
+}
+
+async Task RetrieveWithFilter()
+{
+    FootballLeagueDbContext dbContext = new FootballLeagueDbContext();
+
+    var criteria = "Premier";
+    var allLeagues = await dbContext.Leagues.Where(x => x.Name.Equals(criteria)).ToListAsync();
+    foreach (var league in allLeagues)
+    {
+        Console.WriteLine($"{league.Id} - {league.Name}");
+    }
+
+    Console.WriteLine("Insert League Name");
+    var name = Console.ReadLine();
+
+    var leagues = await dbContext.Leagues.Where(x => x.Name.Equals(name)).ToListAsync();
+    foreach (var league in leagues)
+    {
+        Console.WriteLine($"{league.Id} - {league.Name}");
+    }
+
+    Console.WriteLine("Insert League Name Criteria");
+    string leagueNameCriteria = Console.ReadLine() ?? "";
+
+    var leagues2 = await dbContext.Leagues.Where(x => x.Name.Contains(leagueNameCriteria)).ToListAsync();
+    foreach (var league in leagues)
+    {
+        Console.WriteLine($"{league.Id} - {league.Name}");
+    }
+
+    Console.WriteLine("Insert Second League Name Criteria");
+    string leagueNameCriteria2 = Console.ReadLine() ?? "";
+
+    var leagues3 = await dbContext.Leagues.Where(x => x.Name.Contains(leagueNameCriteria2)).ToListAsync();
+    foreach (var league in leagues)
+    {
+        Console.WriteLine($"{league.Id} - {league.Name}");
+    }
+}
+
+await Test();
