ebd79f3586e0485be0357c13bd5a32e4e580f68a
Reynaldo Rodriguez <rodriguezreynaldo1698@outlook.com>
2022-05-20 11:44:55 -0600

    Added aditional configurations to Database Context to Log commands and show parameters information

--- EntityCourse.ConsoleApp/Program.cs
+++ EntityCourse.ConsoleApp/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EntityCourse.Data.DatabaseContext;
+using EntityCourse.Domain.Models;
+
+void Test()
+{
+    FootballLeagueDbContext dbContext = new FootballLeagueDbContext();
+    var record = new League("Geek League");
+    dbContext.Leagues.Add(record);
+    dbContext.SaveChanges();
+
+    Console.ReadKey();
+}
+
+
+Test();
--- EntityCourse.Data/DatabaseContext/FootballLeagueDbContext.cs
+++ EntityCourse.Data/DatabaseContext/FootballLeagueDbContext.cs
@@ -1,16 +1,21 @@
 using EntityCourse.Domain.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
-namespace EntityCourse.Data.DatabaseContext;
-
-public class FootballLeagueDbContext : DbContext
+namespace EntityCourse.Data.DatabaseContext
 {
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    public class FootballLeagueDbContext : DbContext
     {
-        optionsBuilder.UseSqlServer("Data Source=localhost; Initial Catalog=FootballLeague; User=sa; Password=<U53rn@m3kyn6>");
-    }
 
-    public DbSet<Team> Teams => Set<Team>();
-    public DbSet<League> Leagues => Set<League>();
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost; Initial Catalog=FootballLeague; User=sa; Password=<U53rn@m3kyn6>")
+            .LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Command.Name }, LogLevel.Information)
+            .EnableSensitiveDataLogging();
+        }
+
+        public DbSet<Team> Teams => Set<Team>();
+        public DbSet<League> Leagues => Set<League>();
+    }
 }
