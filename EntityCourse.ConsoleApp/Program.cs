a4ab089bcc834c89df7a2f208578c181f5f5ec60
Reynaldo Rodriguez <rodriguezreynaldo1698@outlook.com>
2022-05-20 14:57:18 -0600

    Added insert statements, single, many and nested

--- EntityCourse.ConsoleApp/Program.cs
+++ EntityCourse.ConsoleApp/Program.cs
@@ -8,8 +8,59 @@ void Test()
     dbContext.Leagues.Add(record);
     dbContext.SaveChanges();
 
+    InsertManyTeams();
+    InsertRelated();
     Console.ReadKey();
 }
 
+void InsertManyTeams()
+{
+    FootballLeagueDbContext dbContext = new FootballLeagueDbContext();
+    var league = new League("Serie A");
+
+    dbContext.Leagues.Add(league);
+    dbContext.SaveChanges();
+
+    var teams = new List<Team>() {
+        new Team("Juventus") {
+            LeagueId = league.Id
+        },
+        new Team("Internazzionales") {
+            League = league
+        },
+        new Team("Torino") {
+            League = league
+        }
+    };
+
+    dbContext.AddRange(teams);
+    dbContext.SaveChanges();
+}
+
+void InsertRelated()
+{
+
+    FootballLeagueDbContext dbContext = new FootballLeagueDbContext();
+    var league = new League("Premier")
+    {
+        Teams = new List<Team>(){
+            new Team("Everton"),
+            new Team("Palace"),
+            new Team("Chelsea")
+        }
+    };
+
+    dbContext.Add(league);
+    dbContext.SaveChanges();
+
+    var team = new Team("Abc")
+    {
+        League = new League("Random League")
+
+    };
+
+    dbContext.Add(team);
+    dbContext.SaveChanges();
+}
 
 Test();
--- EntityCourse.Domain/Models/League.cs
+++ EntityCourse.Domain/Models/League.cs
@@ -14,4 +14,6 @@ public class League
 
     public int Id { get; set; }
     public string Name { get; set; }
+
+    public virtual ICollection<Team> Teams { get; set; }
 }
