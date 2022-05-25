8fb18f22e1cbe6916c5dbe94995bc414984b8569
Reynaldo Rodriguez <rodriguezreynaldo1698@outlook.com>
2022-05-25 11:11:09 -0600

    Adding matches migration

---
+++ EntityCourse.Data/Migrations/20220524215247_Adding_Matches.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EntityCourse.Data.Migrations
+{
+    public partial class Adding_Matches : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
